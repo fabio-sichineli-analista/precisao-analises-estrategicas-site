59c38c94ddb4a970956126c455d7672efc7c57d3
Fábio Sichineli <snfabio27@gmail.com>
2026-03-03 20:04:43 -0300

    Add minimal App component for client/src

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Precisão Análises Estratégicas</h1>
+      <p>Site em construção</p>
+    </div>
+  );
+}
